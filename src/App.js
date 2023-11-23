5ffa6a67e236641b95c1fb29396b3dbcbaf3bb2f
Andrew Wihardja <andrewadi96@gmail.com>
2023-11-23 14:24:47 -0800

    Initial commit. Included features: picking random phrase & displaying random phrase as *'s.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import phraseList from "./static/phrases.json";
 
-function App() {
+export default function App() {
+  const [randomPhrase, setRandomPhrase] = useState("Original phrase");
+  const [displayedPhrase, setDisplayedPhrase] = useState("");
+
+  useEffect(() => {
+    setDisplayedPhrase(hideRandomPhrase(randomPhrase));
+  }, [randomPhrase]);
+
+  const handleClick = () => {
+    const rdn = getRandomPhrase();
+    console.log(rdn);
+    setRandomPhrase(rdn);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{randomPhrase}</p>
+      <p>{displayedPhrase}</p>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
 
-export default App;
+function getRandomPhrase() {
+  const randomNumber = Math.random(); // Generate a random decimal between 0 and 1
+  const scaledNumber = Math.floor(randomNumber * phraseList.length);
+  return phraseList[scaledNumber];
+}
+
+function hideRandomPhrase(phrase) {
+  return phrase.replace(/[a-zA-Z]/g, "*"); // Replaces phrase with *'s.
+}
